0a667053c4763a3d629201197cb8cc5ac9ec4f70
DamonSong <xgyb12458@163.com>
2018-12-15 16:31:15 +0800

    完善基础代码

--- dm-shared/src/main/java/com/damon/shared/utils/ApplicationUtils.java
+++ dm-shared/src/main/java/com/damon/shared/utils/ApplicationUtils.java
@@ -1,9 +1,9 @@
 package com.damon.shared.utils;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.net.UnknownHostException;
+import org.springframework.util.ObjectUtils;
+
+import java.net.*;
+import java.util.Enumeration;
 
 /**
  * @author Damon S.
@@ -15,7 +15,7 @@ public final class ApplicationUtils {
      **/
     public static String findLocalMac() throws SocketException, UnknownHostException {
         //获取网卡，获取地址
-        InetAddress address = InetAddress.getLocalHost();
+        InetAddress address = findInetAddress();
         byte[] mac = NetworkInterface.getByInetAddress(address).getHardwareAddress();
 
         StringBuilder builder = new StringBuilder();
@@ -28,6 +28,48 @@ public final class ApplicationUtils {
             builder.append(s.length() == 1 ? 0 + s : s);
         }
         // 把字符串所有小写字母改为大写成为正规的mac地址并返回
-        return builder.toString().toUpperCase().replaceAll("-", "");
+        return builder.toString().toUpperCase();
+    }
+
+    /**
+     * 获取本地 IP 地址
+     **/
+    public static String findLocalIP() throws SocketException, UnknownHostException {
+        String ipAddress = null;
+
+        InetAddress address = findInetAddress();
+        if (!ObjectUtils.isEmpty(address)) {
+            ipAddress = address.getHostAddress();
+        }
+        return ipAddress;
+    }
+
+    private static InetAddress findInetAddress() throws SocketException, UnknownHostException {
+        InetAddress address = null;
+
+        Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (allNetInterfaces.hasMoreElements()) {
+            NetworkInterface netInterface = allNetInterfaces.nextElement();
+            Enumeration addresses = netInterface.getInetAddresses();
+
+            while (addresses.hasMoreElements()) {
+                InetAddress candidateAddress = (InetAddress) addresses.nextElement();
+                if (!ObjectUtils.isEmpty(address)
+                        && address instanceof Inet4Address
+                        && !address.isLoopbackAddress()
+                        && address.isSiteLocalAddress()) {
+                    address = candidateAddress;
+                    break;
+                }
+            }
+        }
+        // 如果没有发现 non-loopback 地址.只能用最次选的方案
+        if (ObjectUtils.isEmpty(address)) {
+            address = InetAddress.getLocalHost();
+            if (ObjectUtils.isEmpty(address)) {
+                throw new UnknownHostException("The JDK InetAddress.getLocalHost() method unexpectedly returned null.");
+            }
+        }
+        return address;
     }
 }
---
+++ dm-shared/src/test/java/com/damon/shared/common/test/IdFactoryTester.java
@@ -0,0 +1,43 @@
+package com.damon.shared.common.test;
+
+import com.damon.shared.common.IdFactory;
+import com.damon.shared.utils.ApplicationUtils;
+import org.junit.Test;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * @author Damon S.
+ */
+public class IdFactoryTester {
+
+    @Test
+    public void testNextId() {
+        System.out.println("<<<<<<<<<雪花ID[WorkerId = 0]: " + IdFactory.instance().nextId());
+        System.out.println("<<<<<<<<<雪花ID[WorkerId = 2]: " + IdFactory.instance().nextId(IdFactoryTester.class));
+        System.out.println("<<<<<<<<<UUID[WorkerId = null]: " + IdFactory.instance().nextUID());
+    }
+
+    @Test
+    public void testConcurrentNextId() {
+        Runnable test =
+                () -> System.out.println(Thread.currentThread().getName() + ": " +
+                    IdFactory.instance().nextId(IdFactoryTester.class));
+
+        int loop = 100;
+        while (loop-- > 0) {
+            new Thread(test).start();
+        }
+    }
+
+    @Test
+    public void testFindLocalIP() {
+        try {
+            System.out.println("<<<<<<<<<本机MAC: " + ApplicationUtils.findLocalMac());
+            System.out.println("<<<<<<<<<本机IP: " + ApplicationUtils.findLocalIP());
+        } catch (SocketException | UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
